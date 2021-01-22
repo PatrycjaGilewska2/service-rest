dff346a5efa7130a5fbe451cf48307721a2153f9
PGILEWSK <Niemiecki123>
2021-01-22 21:35:14 +0100

    init commit

---
+++ src/main/java/com/pgilewsk/service/WebServiceRestApplication.java
@@ -0,0 +1,13 @@
+package com.pgilewsk.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgilewsk/service/WebServiceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgilewsk.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
